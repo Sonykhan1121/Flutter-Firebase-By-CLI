21eba6a1ab468629d7cc83b78213d64a6c884150
Sidratul Montaha <sonykhan1121@gmail.com>
2025-02-10 22:11:28 +0600

    Add google sign in

--- lib/pages/signuppage.dart
+++ lib/pages/signuppage.dart
@@ -1,21 +1,19 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'homepage.dart';
 import 'loginpage.dart';
 
 class Signuppage extends StatefulWidget {
-
-
-   Signuppage({super.key});
+  Signuppage({super.key});
 
   @override
   State<Signuppage> createState() => _SignuppageState();
 }
 
 class _SignuppageState extends State<Signuppage> {
-
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -31,48 +29,100 @@ class _SignuppageState extends State<Signuppage> {
 
     super.dispose();
   }
+
   void registration() async {
     if (_formKey.currentState!.validate()) {
       setState(() {
         _isProcessing = true;
       });
-      showDialog(context: context,
+      showDialog(
+        context: context,
         barrierDismissible: false,
-        builder: (context)=>Center(child: CircularProgressIndicator(),),);
+        builder: (context) => Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
 
-
-      try{
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailController.text,password: _passwordController.text);
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: _emailController.text,
+                password: _passwordController.text);
         Navigator.pop(context);
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Successfully registered')));
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>Homepage(name: _nameController.text,)));
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Successfully registered')));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Homepage(
+                      name: _nameController.text,
+                    )));
+      } on FirebaseAuthException catch (e) {
+        Navigator.pop(context);
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Please Enter a Strong Password')));
+        } else if (e.code == 'email-already-in-use') {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('Email already in use')));
+        }
+      } finally {
+        setState(() {
+          _isProcessing = false;
+        });
       }
-      on FirebaseAuthException catch(e)
-    {
-      Navigator.pop(context);
-      if(e.code=='weak-password')
-        {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Please Enter a Strong Password')));
-        }
-      else if(e.code=='email-already-in-use')
-        {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Email already in use')));
-        }
-    }
-    finally{
-      setState(() {
-        _isProcessing = false;
-      });
-    }
-
-
     }
   }
+  Future<void> _signInWithGoogle() async {
+    setState(() {
+      _isProcessing = true; // Show loading indicator
+    });
+    try {
+      // 1. Sign in with Google
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      if (googleUser != null) {
+        // 2. Get the Google Auth Credentials
+        final GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
+
+        // 3. Create a new Firebase credential with the Google Auth Credentials
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken,
+          idToken: googleAuth?.idToken,
+        );
+
+        // 4. Once signed in, return the UserCredential
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+        // Navigate to the home page (or wherever you want to go)
+        Navigator.pushReplacement( // Use pushReplacement to avoid going back to signup
+            context,
+            MaterialPageRoute(
+                builder: (context) => Homepage(
+                  name: userCredential.user?.displayName ?? "User", // Provide a default name
+                )));
+
+
+      }
+    } catch (e) {
+      print("Google Sign-in Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to sign in with Google')),
+      );
+    } finally {
+      setState(() {
+        _isProcessing = false; // Hide loading indicator
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+
           child: Form(
             key: _formKey,
             child: Column(
@@ -194,8 +244,8 @@ class _SignuppageState extends State<Signuppage> {
                     ),
                   ),
                   child: TextFormField(
-                    validator: (value){
-                      if(value==null || value!.isEmpty){
+                    validator: (value) {
+                      if (value == null || value!.isEmpty) {
                         return "Please Enter Name";
                       }
                       return null;
@@ -231,8 +281,8 @@ class _SignuppageState extends State<Signuppage> {
                     ),
                   ),
                   child: TextFormField(
-                    validator: (value){
-                      if(value==null || value!.isEmpty){
+                    validator: (value) {
+                      if (value == null || value!.isEmpty) {
                         return "Please Enter Email";
                       }
                       return null;
@@ -268,8 +318,8 @@ class _SignuppageState extends State<Signuppage> {
                     ),
                   ),
                   child: TextFormField(
-                    validator: (value){
-                      if(value==null || value!.isEmpty){
+                    validator: (value) {
+                      if (value == null || value!.isEmpty) {
                         return "Please Enter Password";
                       }
                       return null;
@@ -288,9 +338,11 @@ class _SignuppageState extends State<Signuppage> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     ElevatedButton(
-                      onPressed:_isProcessing?null: () {
-                        registration();
-                      },
+                      onPressed: _isProcessing
+                          ? null
+                          : () {
+                              registration();
+                            },
                       child: Text('Create'),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
@@ -300,8 +352,10 @@ class _SignuppageState extends State<Signuppage> {
                     ),
                     ElevatedButton(
                       onPressed: () {
-                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Loginpage()));
-
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Loginpage()));
                       },
                       child: Text('Login'),
                       style: ElevatedButton.styleFrom(
@@ -311,6 +365,16 @@ class _SignuppageState extends State<Signuppage> {
                       ),
                     ),
                   ],
+                ),
+                GestureDetector(
+                  onTap: _isProcessing ? null : _signInWithGoogle, // Disable button while processing
+                  child: _isProcessing // Show indicator if processing
+                      ? CircularProgressIndicator()
+                      : Image.asset(
+                    "assets/google.jpg",
+                    width: 50,
+                    height: 50,
+                  ),
                 ),
               ],
             ),
